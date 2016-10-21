f730530c2cb8c6ce8e7042d7629a47cf5f06d604
manhnd <manh.it.it.it@gmail.com>
2016-10-21 14:41:06 +0700

    change package

---
+++ app/src/main/java/me/feedplanner/model/imageInsta.java
@@ -0,0 +1,21 @@
+package me.feedplanner.model;
+
+/**
+ * Created by SMR on 9/8/2016.
+ */
+public class imageInsta {
+    private String url;
+
+    public imageInsta(String u) {
+        this.url = u;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl (String u) {
+        this.url = u;
+    }
+
+}
